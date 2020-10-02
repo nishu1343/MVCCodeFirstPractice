c8017f0c488ab9c10a5461ead05cc3300c9bf42d
nishu <konetinishanth@gmail.com>
2020-10-02 19:19:11 -0400

    Add project files.

---
+++ Code-First-Nishu.Model/MyFirstTable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace Code_First_Nishu.Model
+{
+    
+    public class MyFirstTable
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+}
---
+++ Code-First-Nishu.Repository/CDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Code_First_Nishu.Model;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_First_Nishu.Repository
+{
+    public class CDBContext : DbContext
+    {
+        public CDBContext() : base("stringDBContext")
+        {
+
+        }
+        public DbSet<MyFirstTable> MyFirstTable { get; set; }
+    }
+}
